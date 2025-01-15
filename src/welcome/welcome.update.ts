23b587aa4e05ffdc760f0f90c39a89c74bfbb36a
devilzeros <devilzeros@outlook.co.th>
2025-01-15 14:12:56 +0700

    set welcome

--- src/welcome/welcome.update.ts
+++ src/welcome/welcome.update.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Context, On, ContextOf } from 'necord';
-import { AttachmentBuilder } from 'discord.js';
+import { AttachmentBuilder, TextBasedChannel } from 'discord.js';
 import { generateImage } from '../utils/generateImage';
 import { ServerRepository } from 'src/repository/server';
 
@@ -14,19 +14,29 @@ export class WelcomeUpdate {
     @Context() [member]: ContextOf<'guildMemberAdd'>,
   ) {
     try {
-      const server = await this.serverRepository.getServerById(member.guild.id);
-      console.log(18, server);
-
       const buffer = await generateImage(member);
       const image = new AttachmentBuilder(buffer, { name: 'welcome.png' });
-      const channel = await member.guild.channels.fetch(server.welcomechannel);
+      const server = await this.serverRepository.getServerById(member.guild.id);
+      if (!server) {
+        this.logger.warn(`Server with ID ${member.guild.id} is not registered in the database.`);
+        return;
+      }
 
-      if (channel && channel.isTextBased()) {
+      if (!server.welcomechannel) {
+        this.logger.warn(`No welcome channel configured for server ID ${member.guild.id}`);
+        return;
+      }
+      const channel = await member.guild.channels.fetch(server.welcomechannel) as TextBasedChannel;
+      if (channel && channel.isTextBased() && 'send' in channel) {
         await channel.send({
           files: [image],
-          content: `${member.toString()}`,
+          content: `🎉 ยินดีต้อนรับ ${member.toString()} เข้าสู่เซิร์ฟเวอร์!`,
         });
+      } else {
+        this.logger.warn(`The channel is not text-based or does not support sending messages.`);
       }
+
+    
     } catch (error) {
       this.logger.error('Failed to send welcome message', error);
     }

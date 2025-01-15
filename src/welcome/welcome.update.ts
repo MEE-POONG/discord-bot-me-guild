d350002396981fd0397abac387e56b93719ecc19
devilzeros <devilzeros@outlook.co.th>
2025-01-15 13:08:51 +0700

    check code welcome

--- src/welcome/welcome.update.ts
+++ src/welcome/welcome.update.ts
@@ -2,19 +2,24 @@ import { Injectable, Logger } from '@nestjs/common';
 import { Context, On, ContextOf } from 'necord';
 import { AttachmentBuilder } from 'discord.js';
 import { generateImage } from '../utils/generateImage';
+import { ServerRepository } from 'src/repository/server';
 
 @Injectable()
 export class WelcomeUpdate {
   private readonly logger = new Logger(WelcomeUpdate.name);
+  constructor(private readonly serverRepository: ServerRepository) { }
 
   @On('guildMemberAdd')
   public async onGuildMemberAdd(
     @Context() [member]: ContextOf<'guildMemberAdd'>,
   ) {
     try {
+      const server = await this.serverRepository.getServerById(member.guild.id);
+      console.log(18, server);
+
       const buffer = await generateImage(member);
       const image = new AttachmentBuilder(buffer, { name: 'welcome.png' });
-      const channel = await member.guild.channels.fetch('1314455560841859124');
+      const channel = await member.guild.channels.fetch(server.welcomechannel);
 
       if (channel && channel.isTextBased()) {
         await channel.send({

9f91bc108064d1f5b9a6d7cbe45c9694f6e3ec23
warayut teakrathok <warayut2555@gmail.com>
2025-02-20 21:43:01 +0700

    slash

--- src/welcome/welcome.update.ts
+++ src/welcome/welcome.update.ts
@@ -1,13 +1,19 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Context, On, ContextOf } from 'necord';
-import { AttachmentBuilder, TextBasedChannel } from 'discord.js';
+import {
+  Context,
+  On,
+  ContextOf,
+  SlashCommand,
+  SlashCommandContext,
+} from 'necord';
+import { AttachmentBuilder, GuildMember, TextBasedChannel } from 'discord.js';
 import { generateImage } from '../utils/generateImage';
 import { ServerRepository } from 'src/repository/server';
 
 @Injectable()
 export class WelcomeUpdate {
   private readonly logger = new Logger(WelcomeUpdate.name);
-  constructor(private readonly serverRepository: ServerRepository) { }
+  constructor(private readonly serverRepository: ServerRepository) {}
 
   @On('guildMemberAdd')
   public async onGuildMemberAdd(
@@ -18,27 +24,41 @@ export class WelcomeUpdate {
       const image = new AttachmentBuilder(buffer, { name: 'welcome.png' });
       const server = await this.serverRepository.getServerById(member.guild.id);
       if (!server) {
-        this.logger.warn(`Server with ID ${member.guild.id} is not registered in the database.`);
+        this.logger.warn(
+          `Server with ID ${member.guild.id} is not registered in the database.`,
+        );
         return;
       }
 
       if (!server.welcomechannel) {
-        this.logger.warn(`No welcome channel configured for server ID ${member.guild.id}`);
+        this.logger.warn(
+          `No welcome channel configured for server ID ${member.guild.id}`,
+        );
         return;
       }
-      const channel = await member.guild.channels.fetch(server.welcomechannel) as TextBasedChannel;
+      const channel = (await member.guild.channels.fetch(
+        server.welcomechannel,
+      )) as TextBasedChannel;
       if (channel && channel.isTextBased() && 'send' in channel) {
         await channel.send({
           files: [image],
           content: `🎉 ยินดีต้อนรับ ${member.toString()} เข้าสู่เซิร์ฟเวอร์!`,
         });
       } else {
-        this.logger.warn(`The channel is not text-based or does not support sending messages.`);
+        this.logger.warn(
+          `The channel is not text-based or does not support sending messages.`,
+        );
       }
-
-    
     } catch (error) {
       this.logger.error('Failed to send welcome message', error);
     }
+  }
+
+  @SlashCommand({
+    name: 'test-welcome',
+    description: 'ตั้งค่าสำหรับการส่งข้อความยินดีต้อนรับ',
+  })
+  public async welcomeCommand(@Context() [interaction]: SlashCommandContext) {
+    await this.onGuildMemberAdd([interaction.member as GuildMember]);
   }
 }
